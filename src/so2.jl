e3d577c1fa3473d31522abc0c5c4d837f2c46c7a
Amin Abouee <Amin.Abouee@gmail.com>
2019-03-17 16:54:12 +0100

    SO2 generator and transfromation added

--- src/so2.jl
+++ src/so2.jl
@@ -1,24 +1,31 @@
 export SO2,
         matrix,
-        generator
+        generator,
+        transformation
 
 using StaticArrays
 using LinearAlgebra
 
 mutable struct SO2{T<:AbstractFloat}
     DoF::Int8
-    tangent::AbstractFloat
+    tangent::T
+    generator::SMatrix{2, 2, T}
 
     function SO2(tangent::AbstractFloat)
-        # DoF = Int(8)
-        new{typeof(tangent)}(Int8(1), tangent)
+        generator = SMatrix{2, 2, typeof(tangent)}(0, -1, 1, 0)
+        new{typeof(tangent)}(Int8(1), tangent, generator)
     end
 end
 
-function generator()
-    return SMatrix{2, 2, AbstractFloat}(0, -1, 1, 0)
+function generator(group::SO2)
+    return group.generator
 end
 
 function matrix(group::SO2)
-    return group.tangent * SMatrix{2, 2, AbstractFloat}(0, -1, 1, 0)
+    return group.tangent * SMatrix{2, 2, typeof(group.tangent)}(0, -1, 1, 0)
+end
+
+function transformation(group::SO2)
+    # println(typeof(group.tangent))
+    return exp.(group.tangent * group.generator)
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -7,6 +7,7 @@ using StaticArrays
     so = SO2(Float16(3.2))
     @test typeof(so.tangent) == Float16
     @test so.DoF == Int16(1)
-    @test generator() == SMatrix{2,2,Float16}(0, -1, 1, 0)
+    @test generator(so) == SMatrix{2,2,Float16}(0, -1, 1, 0)
     @test matrix(so) == SMatrix{2,2,Float16}(0, -3.2, 3.2, 0)
+    @test transformation(so) == exp.(SMatrix{2,2,Float16}(0, -3.2, 3.2, 0))
 end
